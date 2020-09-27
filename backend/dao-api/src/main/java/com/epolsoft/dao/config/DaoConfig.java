599595fe5863e782e2e55528f23e382e2a31eff6
Andrewka-Java <alze.andrey.1997@mail.ru>
2020-09-27 21:55:31 +0300

    Was cleaned from comments DaoConfig

--- backend/dao-api/src/main/java/com/epolsoft/dao/config/DaoConfig.java
+++ backend/dao-api/src/main/java/com/epolsoft/dao/config/DaoConfig.java
@@ -1,17 +1,13 @@
 package com.epolsoft.dao.config;
 
-import com.epolsoft.dao.SvConfDao;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
-import org.mybatis.spring.mapper.MapperFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 import javax.sql.DataSource;
 
@@ -23,14 +19,6 @@ public class DaoConfig {
     @Autowired
     private DataSource dataSource;
 
-//    @Bean
-//    public DataSource dataSource() {
-//        return new EmbeddedDatabaseBuilder()
-//                .setType(EmbeddedDatabaseType.H2)
-//                .addScript("schema.sql")
-//                .addScript("data.sql")
-//                .build();
-//    }
 
     @Bean
     public SqlSessionFactory sqlSessionFactory() throws Exception {
@@ -42,14 +30,6 @@ public class DaoConfig {
         return factory;
     }
 
-//    @Bean
-//    public MapperFactoryBean svConfDao() throws Exception {
-//        MapperFactoryBean mapperFactoryBean = new MapperFactoryBean();
-//        mapperFactoryBean.setMapperInterface(SvConfDao.class);
-//        mapperFactoryBean.setSqlSessionFactory(sqlSessionFactory());
-//
-//        return mapperFactoryBean;
-//    }
 
     @Bean
     public DataSourceTransactionManager transactionManager() {
