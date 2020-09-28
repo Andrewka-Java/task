1cbbbc159b5c8d5cb00a9e852b2014e9bafa74b8
Andrewka-Java <alze.andrey.1997@mail.ru>
2020-09-28 03:09:28 +0300

    some changes in dao-api

--- backend/dao-api/src/main/java/com/epolsoft/dao/config/DaoConfig.java
+++ backend/dao-api/src/main/java/com/epolsoft/dao/config/DaoConfig.java
@@ -3,11 +3,15 @@ package com.epolsoft.dao.config;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 import javax.sql.DataSource;
 
@@ -16,14 +20,21 @@ import javax.sql.DataSource;
 @EnableAutoConfiguration
 public class DaoConfig {
 
-    @Autowired
-    private DataSource dataSource;
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript("schema.sql")
+                .addScript("data.sql")
+                .setName("service")
+                .build();
+    }
 
 
     @Bean
     public SqlSessionFactory sqlSessionFactory() throws Exception {
         SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
-        factoryBean.setDataSource(dataSource);
+        factoryBean.setDataSource(dataSource());
 
         SqlSessionFactory factory = factoryBean.getObject();
 
@@ -33,6 +44,6 @@ public class DaoConfig {
 
     @Bean
     public DataSourceTransactionManager transactionManager() {
-        return new DataSourceTransactionManager(dataSource);
+        return new DataSourceTransactionManager(dataSource());
     }
 }
--- backend/dao-api/src/main/resources/data.sql
+++ backend/dao-api/src/main/resources/data.sql
@@ -1,5 +1,3 @@
-USE `service`;
-
 INSERT INTO sv_conf (`name`, `value`, `desc`) VALUES ('Spring', 'true', 'Spring framework properties');
 INSERT INTO sv_conf (`name`, `value`, `desc`) VALUES ('Play', 'false', 'Play framework properties');
 INSERT INTO sv_conf (`name`, `value`, `desc`) VALUES ('JavaEE', 'true', 'JavaEE properties');
